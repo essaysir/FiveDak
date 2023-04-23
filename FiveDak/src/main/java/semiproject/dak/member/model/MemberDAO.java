e2985d18cc2f0439a178849b85aa71d4149ef9af
Sonjusun <sonjs7554@naver.com>
2023-04-23 16:40:22 +0900

    로그인 하는 ACTION , DAO 편집하기 - 손주선

--- FiveDak/src/main/java/semiproject/dak/member/model/MemberDAO.java
+++ FiveDak/src/main/java/semiproject/dak/member/model/MemberDAO.java
@@ -150,8 +150,94 @@ public class MemberDAO implements InterMemberDAO {
 	
 	@Override
 	public MemberDTO getMemberByLoginMap(Map<String, String> loginMap) throws SQLException {
-		// 로그인 메소드 구현하기
-		return null;
+		MemberDTO mdto = null ;
+		try {
+			conn = ds.getConnection();
+			String sql = "SELECT  MEMBER_NUM , MEMBER_ID , MEMBER_NAME , MEMBER_MOBILE , MEMBER_EMAIL , "+
+					"		   MEMBER_POINT , MEMBER_GENDER , MEMBER_BIRTH , MEMBER_POSTCODE , MEMBER_ADDRESS , "+
+					"		   MEMBER_DETAIL_ADDRESS , MEMBER_TIER_ID , MEMBER_REG_DATE , pwdchangegap "+
+					"        , nvl(lastlogin_time , trunc( months_between(sysdate, registerday) , 0 )) AS lastlogin_gap "+
+					" FROM  "+
+					" ( "+
+					" select MEMBER_NUM , MEMBER_ID, MEMBER_NAME, MEMBER_EMAIL, MEMBER_MOBILE, MEMBER_POSTCODE, MEMBER_ADDRESS, MEMBER_DETAIL_ADDRESS "+
+					"       , MEMBER_GENDER , MEMBER_POINT , MEMBER_BIRTH , MEMBER_TIER_ID , LAST_PWD_CHANGED , MEMBER_REG_DATE  "+
+					"       , to_char(MEMBER_REG_DATE, 'yyyy-mm-dd') AS registerday "+
+					"       , trunc(months_between(sysdate,LAST_PWD_CHANGED) ,0) AS pwdchangegap "+
+					" from tbl_member   "+
+					" where MEMBER_STATUS = 1 and MEMBER_ID = ? and MEMBER_PWD= ? "+
+					" )M "+
+					" CROSS JOIN "+
+					" ( "+
+					" select trunc( months_between(sysdate, max(LOGIN_TIME)) , 0 ) AS lastlogin_time  "+
+					" from member_login_history  "+
+					" where LOGIN_MEMBER_ID = ? "+
+					" )H ";
+			
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, loginMap.get("userid"));
+			pstmt.setString(2, Sha256.encrypt(loginMap.get("pwd")));
+			pstmt.setString(3, loginMap.get("userid"));
+			
+			
+			rs = pstmt.executeQuery();
+			int cnt = 1 ;
+			if (rs.next()) {
+				mdto = new MemberDTO();
+				mdto.setMbrNum(rs.getInt(cnt++));
+				mdto.setMbrId(rs.getString(cnt++));
+				mdto.setMbrName(rs.getString(cnt++));
+				mdto.setMbrMobile(aes.decrypt(rs.getString(cnt++)));
+				mdto.setMbrEmail(aes.decrypt(rs.getString(cnt++)));
+				mdto.setMbrPoint(rs.getInt(cnt++));
+				mdto.setMbrGender(rs.getString(cnt++));
+				mdto.setMbrBirth(rs.getString(cnt++));
+				mdto.setMbrPostcode(rs.getString(cnt++));
+				mdto.setMbrAddress(rs.getString(cnt++));
+				mdto.setMbrDetailAddress(rs.getString(cnt++));
+				mdto.setMbrTierId(rs.getInt(cnt++));
+				mdto.setMbrRegDate(rs.getString(cnt++));
+				
+				
+				
+				if  ( rs.getInt(cnt++) >= 3 ) { // 패스워드 변경한지 3개월이 지났다면 
+					mdto.setRequirePwdChange(true);
+				}
+				
+				if ( rs.getInt(cnt++) >= 12 ) { 
+					// 로그인 한지가 12개월이 지났다면
+					// === tbl_member 테이블의 idle 컬럼의 값을 1로 변경하기
+					mdto.setMbrIdle(1);
+					
+					sql = " update tbl_member set MEMBER_IDLE = 1 "
+				     +    " where MEMBER_ID = ? ";
+					 
+					pstmt = conn.prepareStatement(sql);
+					
+					pstmt.setString(1, loginMap.get("userid"));
+					
+					pstmt.executeUpdate();
+				}// end of if 
+				
+				// === member_login_history(로그인기록) 테이블에 insert 하기 === // 
+				// 휴면 계정이 아닌 경우에만 넣어주어야 한다.
+				if ( mdto.getMbrIdle() != 1) {
+					
+					sql = " insert into member_login_history (LOGIN_MEMBER_ID, IP_ADDRESS) values ( ? , ? ) ";
+					
+					pstmt = conn.prepareStatement(sql);
+					
+					pstmt.executeUpdate();
+				}// end of if 
+				
+			}// end of if 
+			
+		} catch (UnsupportedEncodingException | GeneralSecurityException e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+		
+		return mdto;
 	}
 
 	
